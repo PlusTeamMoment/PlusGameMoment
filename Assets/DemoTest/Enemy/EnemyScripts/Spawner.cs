9c66eddf7d59f7262b2b6e59a2ccd92b18a74aa4
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-06 16:20:19 +0100

    Spawner crash fix + funzionamento implementato

--- Assets/DemoTest/Enemy/EnemyScripts/Spawner.cs
+++ Assets/DemoTest/Enemy/EnemyScripts/Spawner.cs
@@ -7,27 +7,40 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float spawnRate;
     [SerializeField] private Enemy enemy;
     [SerializeField] private int maxNumberEnemy;
-    private int enemyCount;
+    private IEnumerator spawner;
     public List<Enemy> activeEnemies;
 
     private void Start()
     {
+        spawner = null;
         StartCoroutine(Spawn());
+    }
+
+    private void Update()
+    {
+        if (activeEnemies.Count < maxNumberEnemy && spawner == null) SpawnerSet();
     }
 
 
     private IEnumerator Spawn()
     {
-        while (true)
-        {
-            if (maxNumberEnemy >= enemyCount)
-                continue;
+        var obj = Instantiate(enemy, transform.position, Quaternion.identity);
+        obj.transform.SetParent(this.transform);
+        activeEnemies.Add(obj);
+        yield return new WaitForSeconds(spawnRate);
+        SpawnerDetroy();
+    }
 
-            var obj = Instantiate(enemy, transform.position, Quaternion.identity);
-            activeEnemies.Add(obj);
-            enemyCount++;
-            yield return new WaitForSeconds(spawnRate);
-        }
+    void SpawnerSet()
+    {
+        spawner = Spawn();
+        StartCoroutine(spawner);
+    }
+
+    void SpawnerDetroy()
+    {
+        StopCoroutine(spawner);
+        spawner = null;
     }
 
 }
