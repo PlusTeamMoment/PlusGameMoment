7e7d244583283d95bb535e39b6dc1e31265458f4
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-13 12:01:05 +0100

    Core Table created with interaction

---
+++ Assets/DemoTest/CoreTable/CoreTableInteraction.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CoreTableInteraction : MonoBehaviour
+{
+    [SerializeField] PlayerControls playerControls;
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] GameObject selected;
+    [SerializeField] GameObject convertMenu;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        selected.SetActive(true);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (playerControls.InteractionButton == true)
+        {
+            convertMenu.SetActive(true);
+            playerMovement.enabled = false;
+        }
+        if (convertMenu.activeInHierarchy && playerControls.CloseButton == true)
+        {
+            playerMovement.enabled = true;
+            convertMenu.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        selected.SetActive(false);
+    }
+}
--- Assets/DemoTest/Player/PlayerScripts/PlayerControls.cs
+++ Assets/DemoTest/Player/PlayerScripts/PlayerControls.cs
@@ -5,11 +5,17 @@ public class PlayerControls : MonoBehaviour, INullablePlayerControls
     public float Horizontal { get; set; }
     public float Vertical { get; set; }
     public bool WeaponMainAttack { get; set; }
+    public bool InteractionButton { get; set; }
+    public bool CloseButton { get; set; }
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
         Vertical = Input.GetAxisRaw("Vertical");
         if (Input.GetButtonDown("Fire1")) WeaponMainAttack = true;
         else WeaponMainAttack = false;
+        if (Input.GetKeyDown(KeyCode.E)) InteractionButton = true;
+        else InteractionButton = false;
+        if (Input.GetKeyDown(KeyCode.Escape)) CloseButton = true;
+        else CloseButton = false;
     }
 }
